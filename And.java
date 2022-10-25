2a0d1778487fae603f0226a4b1f93f7ced96c6fe
Madhucn <112704454+Madhucn@users.noreply.github.com>
2022-10-25 11:45:58 +0530

    Add files via upload

---
+++ And.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class And {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10,num2=5;
+		System.out.println(num1&num2);
+		System.out.println(num1^num2);
+        System.out.println(~num2);
+        System.out.println(num1|num2);
+        }
+
+}
---
+++ demo2.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=6,c=7;
+		System.out.println(a*b%c);
+		System.out.println(a%b*c);
+
+	}
+
+}
